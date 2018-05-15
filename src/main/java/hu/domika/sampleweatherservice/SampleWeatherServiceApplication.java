b67d3a014e4aa547e12f682a8f37bf2fa61ca0c6
Balazs Domonkos <github@domika.hu>
2018-05-15 11:25:13 +0200

    Initial commit

---
+++ src/main/java/hu/domika/sampleweatherservice/SampleWeatherServiceApplication.java
@@ -0,0 +1,12 @@
+package hu.domika.sampleweatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWeatherServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleWeatherServiceApplication.class, args);
+    }
+}
